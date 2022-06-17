3bfd47a918b5e85e688eafffcd743208e975eff4
sharmaishaan21 <78147592+sharmaishaan21@users.noreply.github.com>
2022-06-17 21:59:16 +0530

    save

--- database/db.js
+++ database/db.js
@@ -5,7 +5,7 @@ import mongoose from 'mongoose';
 const Connection = (USERNAME, PASSWORD) => {
     const URL = `mongodb+srv://${USERNAME}:${PASSWORD}@cluster0.cfn4r.mongodb.net/project3?retryWrites=true&w=majority`;
     try {
-        mongoose.connect(URL, { useUnifiedTopology: true, useNewUrlParser: true });
+        mongoose.connect(URL);
         console.log("Database connected successfully");
     } catch (error) {
         console.log('Error while connecting with the database', error);
